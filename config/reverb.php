9037dbb1e285a1a3c4a0d9666920bc3371aee8ba
Zeeshan-Ali <zeeshanali.projectmarker@myvteams.com>
2025-09-03 17:17:12 +0500

    Updated Config file for reverb

--- config/reverb.php
+++ config/reverb.php
@@ -5,6 +5,6 @@ return [
     'host'   => env('REVERB_HOST', '127.0.0.1'),
     'port'   => env('REVERB_PORT', 8080),
     'app_id' => env('REVERB_APP_ID'),
-    'key'    => env('REVERB_KEY'),
-    'secret' => env('REVERB_SECRET'),
+    'key'    => env('REVERB_APP_KEY'),
+    'secret' => env('REVERB_APP_SECRET'),
 ];
